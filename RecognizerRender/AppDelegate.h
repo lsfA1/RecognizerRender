44f1716709959995b44aa5c45f01fe82435c821c
李少锋 <wb59@yingu.com>
2018-11-22 09:34:49 +0800

    Initial Commit

---
+++ RecognizerRender/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecognizerRender
+//
+//  Created by 李少锋 on 2018/11/22.
+//  Copyright © 2018年 李少锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RecognizerRender/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecognizerRender
+//
+//  Created by 李少锋 on 2018/11/22.
+//  Copyright © 2018年 李少锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
